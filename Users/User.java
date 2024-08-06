9554da2b412e426541e94debe7419dc4d22fd53e
DylanS.S <dsegurasevilla@gmail.com>
2024-08-06 16:08:24 -0600

    Se creo clase User
    Se creo la clase User, se agregaron atributos y constructores

---
+++ Users/User.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Users;
+
+/**
+ *
+ * @author DYLAN
+ */
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+        this(null,null,null);
+       
+        
+    }
+    
+    
+}
