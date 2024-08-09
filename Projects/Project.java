13ca07760b13e0025ba56711884f25c5d128d48e
Tony <anconejoro@est.utn.ac.cr>
2024-08-09 16:42:53 -0600

    prueba
    prueba

--- Projects/Project.java
+++ Projects/Project.java
@@ -12,10 +12,10 @@ import java.util.ArrayList;
  * @author DYLAN
  */
 public class Project {
+
     private int id;
     private String name;
     private ArrayList<Task> listTask;
-    
 
     public int getId() {
         return id;
@@ -28,15 +28,14 @@ public class Project {
     public ArrayList<Task> getListTask() {
         return listTask;
     }
-    
+
     public void addTask(Task object) {
         listTask.add(object);
     }
-    
+
     public void deleteTask(int id) {
         listTask.removeIf(task -> task.getId() == id);
     }
-    
 
     public Project(int id, String name, ArrayList<Task> listTask) {
         this.id = id;
@@ -47,16 +46,20 @@ public class Project {
     public Project() {
         this(0, null, new ArrayList<Task>());
     }
-    
-    public int countTask(){
+
+    public int countTask() {
         return this.listTask.size();
     }
-    
-    public completeTask{
-    long completeTask= listTask.stream().filter(Task::isComplete.count();
-}
-    
+
+    public completeTask
+
+    {
+        long completeTask = listTask.stream().filter(Task::isComplete.count();
+    }
+
     public complete
-            
-    
+
+    public Task findTask() {
+
+    }
 }
